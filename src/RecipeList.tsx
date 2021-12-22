a55aeb1d2f13b3f9a8dde88c978e12f54ff89874
SamsonChoo <hanye_choo@mymail.sutd.edu.sg>
2021-12-23 03:04:27 +0800

    Add  button for new recipe

--- src/RecipeList.tsx
+++ src/RecipeList.tsx
@@ -8,17 +8,20 @@ interface Props {
 export default function RecipeList(props: Props) {
   const { recipes } = props;
   return (
-    <div>
-      {recipes.map((recipe) => (
-        <Recipe
-          key={recipe.id}
-          name={recipe.name}
-          servings={recipe.servings}
-          cookTime={recipe.cookTime}
-          instructions={recipe.instructions}
-          ingredients={recipe.ingredients}
-        />
-      ))}
-    </div>
+    <>
+      <div>
+        {recipes.map((recipe) => (
+          <Recipe
+            key={recipe.id}
+            name={recipe.name}
+            servings={recipe.servings}
+            cookTime={recipe.cookTime}
+            instructions={recipe.instructions}
+            ingredients={recipe.ingredients}
+          />
+        ))}
+      </div>
+      <button type="button">Add Recipe</button>
+    </>
   );
 }
