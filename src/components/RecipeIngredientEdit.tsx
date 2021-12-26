c3688f9bdee833056c6766f0ef2984ec6b295079
SamsonChoo <hanye_choo@mymail.sutd.edu.sg>
2021-12-26 14:25:50 +0800

    Stylize edit recipe component

--- src/components/RecipeIngredientEdit.tsx
+++ src/components/RecipeIngredientEdit.tsx
@@ -1,9 +1,11 @@
 export default function RecipeIngredientEdit() {
   return (
     <>
-      <input type="text" />
-      <input type="text" />
-      <button type="button">&times;</button>
+      <input type="text" className="recipe-edit__input" />
+      <input type="text" className="recipe-edit__input" />
+      <button type="button" className="btn btn--danger">
+        &times;
+      </button>
     </>
   );
 }
